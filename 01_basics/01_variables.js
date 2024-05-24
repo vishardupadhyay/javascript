a844d57b297a7f363a798b1efc4ae0e41620f959
vishardupadhyay <143780185+vishardupadhyay@users.noreply.github.com>
2024-05-24 17:06:27 +0000

    learning - types of variables in javascript with use cases

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 9882607992
+let accountEmail = "examplemail@example.com"
+var accoountPassword = "PassKey123" // avoid using var while declaring variable in javascript. because of issue in block & functional scope.
+city = "Random" // this is possible in javascript but using this not recommended
+let accountHolderAddress // If any variable is left without initialization, then it is said that it is undefined.
+
+// accountId = 1234 // not allowed in javascript. because constant
+
+ccountEmail = "samplemail@example.com"
+var accoountPassword = "PassKey45"
+city = "Random 2"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accoountPassword, city, accountHolderAddress])
