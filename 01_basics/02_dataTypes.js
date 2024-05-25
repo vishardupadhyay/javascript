18449c8008c347c6ad3bfab40e2751f12735445a
vishardupadhyay <143780185+vishardupadhyay@users.noreply.github.com>
2024-05-25 08:13:50 +0000

    learning - data types in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS code as newer version.
+
+// alert( 3 + 3 ) // this will throw an error because we are using node.js here not the browser.
+
+let name = "Vishard" // string
+let age = 27 // number
+let isLoggedIn = false // boolean
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value - it is an object
+// undefined
+// symbol => uniques
+// object
+
+console.log(typeof undefined) // undefined - because it's a type.
+console.log(typeof null) // object - because it's an object
