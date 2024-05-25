4a29b98c6ae7bbcf613fdb62394f8572d8221636
vishardupadhyay <143780185+vishardupadhyay@users.noreply.github.com>
2024-05-25 08:28:29 +0000

    learning - conversion of datatype in javascript

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN - Not a number
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1= > true
+// 0 => false
+// "" => false
+// "MyName" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
