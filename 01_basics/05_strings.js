5dd40016623ee2c26d5a5bb01ef6830bcdff8c2c
vishardupadhyay <143780185+vishardupadhyay@users.noreply.github.com>
2024-05-29 18:37:43 +0000

    learning - javascript - added notes on strings.

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -11,7 +11,20 @@ concatenating the string.
 console.log(`Hello my name is ${myName} and my age is ${myAge}.`);
 
 /* The below technique to declare the string allow using multiple methods such as charAt, length etc. */
-const userName = new String("vupadhya") // another way to declaring the string.
+const userName = new String("vupad-hya") // another way to declaring the string.
 
 console.log(userName.charAt(2)) // will print p
-console.log(userName.indexOf(y)) // will print index position of character y
+console.log(userName.indexOf('y')) // will print index position of character - 6
+
+/* Note - the characters at end index are not included as a part of string in both substring and slice methods. */
+const newString = userName.substring(0, 4) // will extract only first four character of the string userName, only acts on positive indexes.
+const anotherString = userName.slice(0, 4) // will also extract only first four characters of the string, but also allows negative indexs.
+
+/*
+    Points to remember
+    1. When negative indexs are supplied to substring method, it is ignored and replaced with 0.
+    2. If startIndex is greater than endIndex. then substring will swap both the indexes with each other but slice will not.
+    3. Positive index will start from 0 whereas negative will start from -1 at the last.
+    4. 
+*/
+console.log(userName.split('-')); // splits the string into array of character based on the splitter.
