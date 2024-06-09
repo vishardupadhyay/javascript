8e63bd2a3585c19e7768142f69b7afd855a43cdc
vishardupadhyay <vishardupadhyay.online@gmail.com>
2024-06-09 10:38:53 +0530

    learning - javascript - started with mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,10 +5,37 @@ if(true) {
     var c = 30
 }
 
-// will throw an error because of inaccessibility
-console.log(a) 
-// will throw an error because of inaccessibility
-console.log(b)
-// will print the value assigned, which is wrong. Variables declared inside the scope must not be accessible outside.
-// That's why using var is not recommended in javascript due to accessibility concerns. 
-console.log(c)
+// // will throw an error because of inaccessibility
+// console.log(a) 
+// // will throw an error because of inaccessibility
+// console.log(b)
+// // will print the value assigned, which is wrong. Variables declared inside the scope must not be accessible outside.
+// // That's why using var is not recommended in javascript due to accessibility concerns. 
+// console.log(c)
+
+function parent(){
+    const userName = "Vishard"
+
+    function child(){
+        const website = "YouTube"
+        console.log(userName) // will print Vishard
+    }
+
+    //console.log(website) // Will throw an error as website is not defined (Because local variable to child function)
+    child() // calling child function
+
+}
+
+//parent()
+
+console.log(addOne(5)) // this will print the value 6 in the console.
+
+function addOne(givenNumber) {
+    return givenNumber + 1;
+}
+
+console.log(addTwo(5)) // this will throw an error (Cannot access 'addTwo' before initialization) due to hoisting.
+
+const addTwo = function(givenNumber){
+    return givenNumber + 1;
+}
